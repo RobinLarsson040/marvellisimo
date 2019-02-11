9d0a0fe8e01377521ce72aac4ae5852999add20d
JonasFisk <Ldr_shadow@hotmail.com>
2019-02-11 13:45:14 +0100

    created marvelservice

--- app/src/main/java/com/example/marvelzomg/services/MarvelService.kt
+++ app/src/main/java/com/example/marvelzomg/services/MarvelService.kt
@@ -1,6 +1,7 @@
-package com.example.marvelzomg.service
+package com.example.marvelzomg.services
 
 import com.example.marvelzomg.models.CharacterDataWrapper
+import com.example.marvelzomg.models.ComicDataWrapper
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -11,18 +12,21 @@ interface MarvelService {
     @GET("characters")
     fun getAllCharacters()
 
-    @GET
+    @GET("characters")
     fun getAllCharactersByName(@Query("nameStartsWith") nameStartsWith: String, @Query("limit") limit: Int,
                              @Query("offset") offset: Int): Observable<CharacterDataWrapper>
 
-    @GET("character/{id}")
-    fun getCharacterById(@Path("id") id: Int)
+    @GET("characters/{id}")
+    fun getCharacterById(@Path("id") id: Int): Observable<CharacterDataWrapper>
 
     @GET("comics")
-    fun getAllComics()
+    fun getAllComics(): Observable<ComicDataWrapper>
 
     @GET("comics")
     fun getAllComicsByName(@Query("nameStartsWith") nameStartsWith: String, @Query("limit") limit: Int,
-                           @Query("offset") offset: Int)
+                           @Query("offset") offset: Int): Observable<ComicDataWrapper>
+
+    @GET("comics/{id}")
+    fun getComicById(@Path("id") id: Int): Observable<CharacterDataWrapper>
 
 }
